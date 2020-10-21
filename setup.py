4b6a76b3e60e0096c9da99d44a274ed6d18cd6b3
Mike Kittridge <mullenkamp1@gmail.com>
2020-10-21 15:13:55 +1300

    update of the water use type category

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 name = 'FlowNaturalisation'
 main_package = 'flownat'
 datasets = 'datasets'
-version = '0.1.8'
+version = '0.1.9'
 descrip = 'Tools to perform flow naturalisation in Caterbury'
 
 # The below code is for readthedocs. To have sphinx/readthedocs interact with
